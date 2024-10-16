e7928e0735cd008cddf6bcabd02f48231c5741da
AryanTapre <taprearyan7@gmail.com>
2024-10-16 15:51:02 +0530

    generic classes in TS

--- one.js
+++ one.js
@@ -1,4 +1,19 @@
 // Generic regular / arrow functions.
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 function greetings(val) {
     console.log("hello, " + val);
 }
@@ -12,5 +27,43 @@ var greetings4 = function (val) {
     console.log("hello " + val);
 };
 greetings4("aryantapre");
-var pass = new Boolean(true);
-console.log(pass);
+// const pass = new Boolean(true);
+// console.log(pass);
+// generic classes
+var Example = /** @class */ (function () {
+    function Example() {
+        console.log("constructor called!.");
+    }
+    Example.prototype.greet = function (person) {
+        console.log("good afternoon, " + person);
+    };
+    return Example;
+}());
+var p1 = new Example();
+p1.greet("aryan-tapre");
+function createObject(instance) {
+    return new instance();
+}
+var p2 = createObject(Example);
+p2.greet("harshad mehta");
+var Person = /** @class */ (function () {
+    function Person() {
+        this.height = 5;
+    }
+    return Person;
+}());
+var Aryan = /** @class */ (function (_super) {
+    __extends(Aryan, _super);
+    function Aryan() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.name = "Aryan";
+        return _this;
+    }
+    return Aryan;
+}(Person));
+var getInstance = function (instance) {
+    return new instance();
+};
+var aryan2 = getInstance(Aryan);
+aryan2.height = 6;
+console.log(aryan2.height);
--- one.ts
+++ one.ts
@@ -21,11 +21,52 @@ interface Greetings2<T> {
     (val:T):void
 }
 
-const greetings4:Greetings2<String> = function(val) {
+type SString = String;
+const greetings4:Greetings2<SString> = function(val) {
     console.log("hello "+val);
 }
 
 greetings4("aryantapre")
 
-const pass = new Boolean(true);
-console.log(pass);
+// const pass = new Boolean(true);
+// console.log(pass);
+
+// generic classes
+
+class Example<Type> {
+    constructor() {
+        console.log("constructor called!.");
+    }
+    greet(person: Type) {
+        console.log("good afternoon, "+person);
+        
+    }
+}
+
+const p1:Example<string> = new Example();
+p1.greet("aryan-tapre");
+
+function createObject<T>(instance: {new():T}): T {
+    return new instance();
+}
+
+const p2:Example<string> = createObject(Example);
+p2.greet("harshad mehta");
+
+class Person {
+    height: number = 5;
+}
+
+class Aryan extends Person {
+    name:string = "Aryan";
+}
+
+const getInstance = <Type extends Person>(instance: {new ():Type}) => {
+    return new instance()
+}
+
+const aryan2:Aryan = getInstance(Aryan);
+aryan2.height = 6;
+console.log(aryan2.height);
+
+
